29cedf7cf606fe6d0a205a8bb11456a6ddd60f84
SreeNath <gootysreenathreddy@gmail.com>
2024-03-14 11:32:30 +0530

    Update interview_shell_command.bash

--- interview_shell_command.bash
+++ interview_shell_command.bash
@@ -1,3 +1,10 @@
 1.How to print a specific line number using linux command
 
 sed -n '50p' example.txt
+
+2.How to check running process in linux
+  a.To list all running processes
+  ps aux
+  b.To list processes owned by a specfic user(replace name with the actual username)
+   ps -u username
+  
